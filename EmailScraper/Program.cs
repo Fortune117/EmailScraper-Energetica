adb244bc04396fe7a8603624f634c17ee7e93b12
Fortune <7847372+Fortune117@users.noreply.github.com>
2024-05-30 12:41:52 +1000

    add very basic query system for employees

--- EmailScraper/Program.cs
+++ EmailScraper/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Linq;
 using LinqToDB;
-using LinqToDB.Data;
-using LinqToDB.DataProvider.SqlServer;
 using LinqToDB.Mapping;
 
 
 namespace EmailScraper
 {
     [Table(Name = "employeelist")]
-    public class EmployeeList
+    public class Employee
     {
-        [Column(Name = "eid")]
+        [Column(Name = "eid", IsPrimaryKey = true)]
         public int EmployeeID { get; set; }
         
         [Column(Name = "firstName")]
@@ -20,7 +18,7 @@ namespace EmailScraper
         [Column(Name = "lastName")]
         public string LastName { get; set; }
         
-        [Column(Name = "Email_id", IsPrimaryKey = true)]
+        [Column(Name = "Email_id")]
         public string Email { get; set; }
         
         [Column(Name = "Email2")]
@@ -39,6 +37,63 @@ namespace EmailScraper
         public string Status { get; set; }
     }
     
+    [Table(Name = "message")]
+    public class Messages
+    {
+        [Column(Name = "mid", IsPrimaryKey = true)]
+        public int MessageID { get; set; }
+        
+        [Column(Name = "sender")]
+        public string Sender { get; set; }
+        
+        [Column(Name = "date")]
+        public DateTime Date { get; set; }
+        
+        [Column(Name = "message_id")]
+        public string InternalMessageID { get; set; }
+        
+        [Column(Name = "subject")]
+        public string Subject { get; set; }
+        
+        [Column(Name = "body")]
+        public string Body { get; set; }
+        
+        [Column(Name = "folder")]
+        public string Folder { get; set; }
+    }
+    
+    [Table(Name = "referenceinfo")]
+    public class ReferenceInfo
+    {
+        [Column(Name = "rfid", IsPrimaryKey = true)]
+        public int ReferenceID { get; set; }
+        
+        [Column(Name = "mid")]
+        public string MessageID { get; set; }
+        
+        [Column(Name = "reference")]
+        public string RecipientType { get; set; }
+    }
+    
+    [Table(Name = "recipientinfo")]
+    public class RecipientInfo
+    {
+        [Column(Name = "rid", IsPrimaryKey = true)]
+        public int RecipientID { get; set; }
+        
+        [Column(Name = "mid")]
+        public string MessageID { get; set; }
+        
+        [Column(Name = "rtype")]
+        public string RecipientType { get; set; }
+        
+        [Column(Name = "rvalue")]
+        public string RecipientValue { get; set; }
+        
+        [Column(Name = "dater")]
+        public string Dater { get; set; }
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
@@ -48,10 +103,18 @@ namespace EmailScraper
 
 // pass configured options to data context constructor
             var db = new DataContext(options);
-            
-            var employeeLists = db.GetTable<EmployeeList>();
 
-            foreach (var employee in employeeLists)
+            Console.WriteLine("Enter a keyword to search for:");
+            var searchTerm = Console.ReadLine();
+            
+            var employeeLists = db.GetTable<Employee>();
+
+            IQueryable<Employee> employeeQuery =
+                from employee in employeeLists
+                where employee.FirstName.Contains(searchTerm)
+                select employee;
+
+            foreach (var employee in employeeQuery)
             {
                 Console.WriteLine(employee.FirstName);
             }
